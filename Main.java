6aa27147b26703d54abd54e0e2f0989c138bdb36
Jacob <jtjonnybravo@gmail.com>
2021-04-12 16:53:23 -0500

    Added Class Account

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ class Main {
     public static void main(String[] args) {
         IDandPass idAndPasswords = new IDandPass(); // initialize id and passwords --- this can be replaced for simply acct numbers instead
         LoginPage loginPage = new LoginPage(idAndPasswords.getLoginInfo()); // generate login page
+
     }
 }
