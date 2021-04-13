4bfbd5ddbea316db8e8237dbb1348706b135a723
Jacob <jtjonnybravo@gmail.com>
2021-04-12 20:05:53 -0500

    Created customer class and getcustomers method to read in the customer file and create customer objects

--- Classes/IDandPass.java
+++ Classes/IDandPass.java
@@ -3,12 +3,30 @@ import java.util.HashMap;
 
 public class IDandPass {
     HashMap<String,String> loginInfo = new HashMap<>();
+    public int password;
+    public String username;
 
     public IDandPass(){
         loginInfo.put("person1", "password");
         loginInfo.put("person2", "password2");
         loginInfo.put("person3", "password3");
         loginInfo.put("a", "a");
+        loginInfo.put(username, String.valueOf(password));
+    }
+
+    public IDandPass(String username, int password)
+    {
+        this.username = username;
+        this.password = password;
+        loginInfo.put(username, String.valueOf(password));
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public HashMap getLoginInfo() {
--- Main.java
+++ Main.java
@@ -1,11 +1,53 @@
 import javax.swing.*;
 import Classes.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 class Main {
     public static void main(String[] args) {
-        IDandPass idAndPasswords = new IDandPass(); // initialize id and passwords --- this can be replaced for simply acct numbers instead
-        LoginPage loginPage = new LoginPage(idAndPasswords.getLoginInfo()); // generate login page
+        getCustomers();
+        //IDandPass idAndPasswords = new IDandPass(); // initialize id and passwords --- this can be replaced for simply acct numbers instead
+        //LoginPage loginPage = new LoginPage(idAndPasswords.getLoginInfo()); // generate login page
 
+
+    }
+
+    //getCustomers method that reads in the customer file and assigns it to a customer class
+    static void getCustomers() {
+        String file = "Database Files/customer.csv";
+        List<Customer> customers = new ArrayList<>();
+        IDandPass idandpass = new IDandPass();
+
+        //Reads Customers file
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = "";
+
+            //Skips first line to ignore the column headings
+            int lineSkip = 0;
+
+            //iterates through the list to assign them to customer
+            while ((line = br.readLine()) != null) {
+                if (lineSkip != 0) {
+                    Customer customer = new Customer(line);
+                    customers.add(customer);
+                    customer.listCustomers();
+                    idandpass = new IDandPass(customer.getUserName(), customer.getPassword());
+
+                }
+                lineSkip++;
+            }
+            customers.get(0).listCustomers();
+            LoginPage loginPage = new LoginPage(idandpass.getLoginInfo());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
