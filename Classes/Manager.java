455f25854b5cde15283d6262a2de315555d21deb
Chy Dewey <61438407+CADewey404@users.noreply.github.com>
2021-04-15 11:54:41 -0500

    Minor Tweaks
    Nothing of major consequence, unless I really messed up.

--- Classes/Manager.java
+++ Classes/Manager.java
@@ -1,24 +1,11 @@
 package Classes;
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.CardLayout;
-import java.awt.Font;
-import java.text.NumberFormat;
-import java.util.HashMap;
-
 public class Manager {
 
-    private String username;
-    private String password;
     public Manager(){
         return;
     }
-
     
-    public String getUserName(){return username;}
-    public String getPassword(){return password;}
     public void listCustomers(){return;}
 
     //Constructor
