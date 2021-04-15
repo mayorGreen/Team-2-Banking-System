1205fc593cb7deeebb59183a52bed84f6cb2f783
Chy Dewey <61438407+CADewey404@users.noreply.github.com>
2021-04-15 11:38:04 -0500

    Minor Tweaks
    Nothing of major importance yet.

--- Classes/Manager.java
+++ Classes/Manager.java
@@ -1,4 +1,29 @@
 package Classes;
 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.CardLayout;
+import java.awt.Font;
+import java.text.NumberFormat;
+import java.util.HashMap;
+
 public class Manager {
+
+    private String username;
+    private String password;
+    public Manager(){
+        return;
+    }
+
+    
+    public String getUserName(){return username;}
+    public String getPassword(){return password;}
+    public void listCustomers(){return;}
+
+    //Constructor
+    public Manager(String userid) {
+        userid = getUserName();
+    }
+
 }
